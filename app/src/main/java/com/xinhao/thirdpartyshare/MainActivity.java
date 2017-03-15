a747d5c8ec56941f088ad5a10c5aed2cc55296f6
Alpha58 <maxionghui58@163.com>
2017-03-15 18:19:01 +0800

    Initial commit

---
+++ app/src/main/java/com/xinhao/thirdpartyshare/MainActivity.java
@@ -0,0 +1,48 @@
+package com.xinhao.thirdpartyshare;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void share(View view) {
+        showShare();
+    }
+
+    private void showShare() {
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        // title标题，印象笔记、邮箱、信息、微信、人人网、QQ和QQ空间使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，仅在Linked-in,QQ和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        //分享网络图片，新浪微博分享网络图片需要通过审核后申请高级写入接口，否则请注释掉测试新浪微博
+        oks.setImageUrl("http://f1.sharesdk.cn/imgs/2014/02/26/owWpLZo_638x960.jpg");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite("ShareSDK");
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+        // 启动分享GUI
+        oks.show(this);
+    }
+}
---
+++ app/src/main/java/com/xinhao/thirdpartyshare/MyApplication.java
@@ -0,0 +1,24 @@
+package com.xinhao.thirdpartyshare;
+
+import android.app.Application;
+
+import cn.sharesdk.framework.ShareSDK;
+
+/**
+ * author           Alpha58
+ * date             2017/03/15
+ * desc	            ${Application}
+ * <p>
+ * upDateAuthor     $Author$
+ * upDate           $Date$
+ * upDateDesc       ${TODO}
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //初始化ShareSDK
+        ShareSDK.initSDK(this);
+    }
+}
